3e1393b6410462fdec3e96edce3c4246e9ab2a00
jkunciene <j.kunciene@gmail.com>
2020-12-02 10:16:25 +0200

    BookList'as

--- src/components/book/Book.jsx
+++ src/components/book/Book.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './book.css';
 
 
-function Book() {
+export default function Book() {
     return (
         <div className='oneBook'>
             <Image/>
@@ -14,9 +14,6 @@ function Book() {
 }
 
 const Image = () => <img src="https://thumb.knygos-static.lt/oQDegotQlH146nsHsWJyDtmkiV0=/fit-in/320x430/filters:cwatermark(static/wm.png,500,75,30)/images/books/1453575/1584446844_D%C5%BEyru_d%C5%BEyru_72_dpi.jpg" alt=""/>
-const Author = () => <h2>Autorius knygos</h2>;
-const Title = () => <h3>Knygos pavadinimas</h3>;
-const Price = () => <p>tik 10 $</p>
-
-
-export default Book
+const Author = () => <h2>Tolstojus</h2>;
+const Title = () => <h3>naujas visiskai</h3>;
+const Price = () => <p>tik 15 $</p>;
